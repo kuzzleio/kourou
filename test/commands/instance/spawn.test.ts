363d9af4b69dea2c727b370a28abf237e97c5e42
Aschen <adrien@maret.email>
2020-06-12 05:47:37 +0200

    test

--- test/commands/instance/spawn.test.ts
+++ test/commands/instance/spawn.test.ts
@@ -2,15 +2,16 @@ import { expect, test } from '@oclif/test'
 
 describe('instance:spawn', () => {
   test
-    .timeout(10000)
+    .timeout(20000)
     .stdout()
     .command(['instance:spawn'])
     .it('Spawns a new Kuzzle', ctx => {
+      console.log(ctx.stdout)
       expect(ctx.stdout).to.contain('Kuzzle version 2 is launching')
     })
 
   test
-    .timeout(10000)
+    .timeout(20000)
     .stdout()
     .command(['instance:spawn', '--version', '1'])
     .it('Spawns a new Kuzzle v1', ctx => {
