05f603a725b4bd690ceb580468764d2566afcd83
Berthier <Berthier.est@gmail.com>
2020-07-08 10:35:15 +0200

    remove logs

--- test/commands/instance/kill.test.ts
+++ test/commands/instance/kill.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@oclif/test'
 import { execSync } from 'child_process'
 
 const TEST_TIMEOUT = 50000
-const PRINT_STDOUT = true
+const PRINT_STDOUT = false
 
 describe('instance:kill', () => {
   test
