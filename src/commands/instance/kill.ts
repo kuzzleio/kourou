fef765e01909ab0f22b0cc3ad660a209974cd448
Berthier <Berthier.est@gmail.com>
2020-07-09 12:33:34 +0200

    log

--- src/commands/instance/kill.ts
+++ src/commands/instance/kill.ts
@@ -7,17 +7,18 @@ import { Kommand } from '../../common'
 export class InstanceLogs extends Kommand {
   static initSdk = false
 
-  static description = 'Stop and remove all the containers of a running kuzzle instance'
+  static description =
+    'Stop and remove all the containers of a running kuzzle instance'
 
   static flags = {
     instance: flags.string({
       char: 'i',
-      description: 'Kuzzle instance name [ex: stack-0]',
+      description: 'Kuzzle instance name [ex: stack-0]'
     }),
     all: flags.boolean({
       char: 'a',
-      description: 'Kill all instances',
-    }),
+      description: 'Kill all instances'
+    })
   }
 
   async runSafe() {
@@ -36,23 +37,32 @@ export class InstanceLogs extends Kommand {
       return
     }
     if (!instance) {
-      const responses: any = await inquirer.prompt([{
+      const responses: any = await inquirer.prompt([
+        {
           name: 'instance',
           message: 'Which kuzzle instance do you want to kill',
           type: 'list',
           choices: instancesList
-      }])
+        }
+      ])
       instance = responses.instance!
-    }
-    else if (!instancesList.includes(instance)) {
-      throw new Error('The instance parameter you setted isn\'t running')
+    } else if (!instancesList.includes(instance)) {
+      throw new Error(
+        `The instance parameter you setted ${instance} isn\'t running`
+      )
     }
     await this.killInstance(instance)
   }
 
   private async killInstance(instanceName: string) {
     const docoFilename = `/tmp/kuzzle-${instanceName}.yml`
-    const instanceKill = execa('docker-compose', ['-f', docoFilename, '-p', instanceName, 'down'])
+    const instanceKill = execa('docker-compose', [
+      '-f',
+      docoFilename,
+      '-p',
+      instanceName,
+      'down'
+    ])
 
     instanceKill.stdout.pipe(process.stdout)
     instanceKill.stderr.pipe(process.stderr)
@@ -64,21 +74,30 @@ export class InstanceLogs extends Kommand {
     let containersListProcess
 
     try {
-      containersListProcess = await execa('docker', ['ps', '--format', '"{{.Names}}"'])
+      containersListProcess = await execa('docker', [
+        'ps',
+        '--format',
+        '"{{.Names}}"'
+      ])
       this.log(JSON.stringify(containersListProcess.stdout))
-    }
-    catch {
-      this.warn('Something went wrong while getting kuzzle running instances list')
+    } catch {
+      this.warn(
+        'Something went wrong while getting kuzzle running instances list'
+      )
       return []
     }
 
-    const containersList: string[] = containersListProcess.stdout.replace(/"/g, '').split('\n')
+    const containersList: string[] = containersListProcess.stdout
+      .replace(/"/g, '')
+      .split('\n')
 
     return containersList
-      .filter(containerName =>
-        (containerName.includes('kuzzle')
-        && !containerName.includes('redis')
-        && !containerName.includes('elasticsearch')))
+      .filter(
+        containerName =>
+          containerName.includes('kuzzle') &&
+          !containerName.includes('redis') &&
+          !containerName.includes('elasticsearch')
+      )
       .map(containerName => containerName.replace('_kuzzle_1', ''))
   }
 }
