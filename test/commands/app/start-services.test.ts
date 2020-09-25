5d00843402c2101b75e51fac4c7b6d0b98a9a052
Aschen <adrien@maret.email>
2020-09-25 10:39:05 +0200

    add tests

---
+++ test/commands/app/start-services.test.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@oclif/test'
+import { execSync } from 'child_process'
+
+const SECOND = 1000
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+describe('app:start-services', () => {
+  test
+    .timeout(50 * SECOND)
+    .stdout({ print: false })
+    .command(['app:start-services'])
+    .finally(() => {
+      execSync('docker stop $(docker ps -aq)')
+    })
+    .it('Spawns Kuzzle v2 services', async (ctx, done) => {
+      await wait(10 * SECOND)
+      expect(ctx.stdout).to.contain('Elasticsearch and Redis are launching')
+      expect(ctx.stdout).to.contain('Elasticsearch and Redis are booting in the background right now')
+      done()
+    })
+})
