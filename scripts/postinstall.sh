6a047f021301fae9c3940afc7ec98ab3500b584e
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-02-09 18:48:18 +0100

    Update postinstall message

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,14 +6,6 @@ if [ -f templates.tgz ];
 fi
 
 echo """
-This version of Kourou embed usage analytics.
-It's fully anonymous and does not track your IP address or other personal information.
-We use our custom analytics system Kepler, you can review the code here:
-https://github.com/kuzzleio/kepler
-
-If you want to turn it off, just run the following commands:
-export KOUROU_ANALYTICS=false
-
-To make it permanent, add the following line to your ~/.bashrc then source it to apply the changes:
-export KOUROU_ANALYTICS=false
+Kourou send anonymous usage info to our open source analytics server Kepler to help us improve the product.
+Set KUZZLE_ANALYTICS=false to disable this behavior.
 """
