a6c2d9d960acf709b5d7abe966d5af8aea6c7c53
Olivier Cavadenti <olivier.cavadenti@gmail.com>
2022-11-03 14:48:55 +0100

    revert api inference deletion

--- src/hooks/command_not_found/api-action.ts
+++ src/hooks/command_not_found/api-action.ts
@@ -1,8 +1,31 @@
 import chalk from "chalk";
 import { Hook } from "@oclif/config";
+import Api from "../../commands/api";
 
 /**
- * Hooks that checks if the command is not found and suggest api kourou command.
+ * Hooks that run the corresponding API action with api.
+ *
+ * Example:
+ *  - kourou document:create -i index -c collection --id foobar-1 --body '{}'
+ *
+ * Index, collection, ID and body can be infered if they are
+ * passed as argument in one of the following order:
+ *  - <index> <collection> <id> <body>
+ *  - <index> <collection> <body>
+ *  - <index> <collection> <id>
+ *  - <index> <collection>
+ *  - <index>
+ *  - <body>
+ *
+ * This is mainly to match easily methods from the document, bulk, realtime
+ * and collection controllers.
+ *
+ * If one of the infered arguments is passed as a flag, then Kourou will
+ * use the flag value and will not try to infere arguments.
+ *
+ * Example:
+ *  - kourou document:create index collection foobar-1 '{}'
+ *  - kourou bulk:import index collection '{bulkData: []}'
  */
 
 const hook: Hook<"command_not_found"> = async function (opts) {
@@ -11,12 +34,76 @@ const hook: Hook<"command_not_found"> = async function (opts) {
   if (!controller || !action) {
     return;
   }
-  const apiBody = process.argv.slice(2).join(" ");
+
   this.log(
-    chalk.yellow(
-      `[ℹ] Unknown command "${opts.id}", if you want to execute an API action, use "kourou api ${apiBody}".`
-    )
+    chalk.yellow(`[ℹ] Unknown command "${opts.id}", fallback to API action`)
   );
+
+  const args = process.argv.slice(3);
+  const commandArgs = [opts.id];
+
+  // first positional argument (index or body)
+  if (args[0] && args[0].charAt(0) !== "-") {
+    if (args[0].includes("{") && !args.includes("--body")) {
+      commandArgs.push("--body");
+      commandArgs.push(args[0]);
+
+      args.splice(0, 1);
+    } else if (!args.includes("--index") && !args.includes("-i")) {
+      commandArgs.push("-i");
+      commandArgs.push(args[0]);
+
+      args.splice(0, 1);
+    }
+  } else {
+    const exitCode = await Api.run([...commandArgs, ...args]);
+    process.exit(exitCode);
+  }
+
+  // 2th positional argument (collection)
+  if (
+    args[0] &&
+    args[0].charAt(0) !== "-" &&
+    !args.includes("-c") &&
+    !args.includes("--collection")
+  ) {
+    commandArgs.push("-c");
+    commandArgs.push(args[0]);
+
+    args.splice(0, 1);
+  } else {
+    const exitCode = await Api.run([...commandArgs, ...args]);
+    process.exit(exitCode);
+  }
+
+  // 3th positional argument (_id or body)
+  if (args[0] && args[0].charAt(0) !== "-") {
+    if (args[0].includes("{") && !args.includes("--body")) {
+      commandArgs.push("--body");
+      commandArgs.push(args[0]);
+
+      args.splice(0, 1);
+    } else if (!args.includes("--id")) {
+      commandArgs.push("--id");
+      commandArgs.push(args[0]);
+
+      args.splice(0, 1);
+    }
+  } else {
+    const exitCode = await Api.run([...commandArgs, ...args]);
+    process.exit(exitCode);
+  }
+
+  // 4th positional argument (body)
+  if (args[0] && args[0].charAt(0) !== "-" && !commandArgs.includes("--body")) {
+    commandArgs.push("--body");
+    commandArgs.push(args[0]);
+
+    args.splice(0, 1);
+  }
+
+  const exitCode = await Api.run([...commandArgs, ...args]);
+  process.exit(exitCode);
 };
 
 export default hook;
