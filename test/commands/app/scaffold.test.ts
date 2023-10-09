fc06201b21b9c538b53c189fd5ee956961799aaa
rolljee <n.riquelmebareiro@gmail.com>
2023-10-09 19:30:23 +0200

    Update tests with second flavor

--- test/commands/app/scaffold.test.ts
+++ test/commands/app/scaffold.test.ts
@@ -5,10 +5,27 @@ import should from "should";
 import { kourou } from "../../support";
 
 describe("app:scaffold", () => {
-  it("creates desired files and install packages", () => {
+  it("creates desired files and install packages of generic", () => {
     execSync("rm -rf blackmesa/");
 
     kourou("app:scaffold blackmesa");
+
+    should(fs.existsSync("./blackmesa/.eslintignore")).be.eql(true);
+    should(fs.existsSync("./blackmesa/.eslintrc.json")).be.eql(true);
+    should(fs.existsSync("./blackmesa/.gitignore")).be.eql(true);
+    should(fs.existsSync("./blackmesa/Dockerfile")).be.eql(true);
+    should(fs.existsSync("./blackmesa/docker-compose.yml")).be.eql(true);
+    should(fs.existsSync("./blackmesa/app.ts")).be.eql(true);
+    should(fs.existsSync("./blackmesa/package.json")).be.eql(true);
+    should(fs.existsSync("./blackmesa/README.md")).be.eql(true);
+    should(fs.existsSync("./blackmesa/tsconfig.json")).be.eql(true);
+    should(fs.existsSync("./blackmesa/jest.config.ts")).be.eql(true);
+  }).timeout("60s");
+
+  it("creates desired files and install packages of IoT platform", () => {
+    execSync("rm -rf blackmesa/");
+
+    kourou("app:scaffold blackmesa --flavor iot-platform");
 
     should(fs.existsSync("./blackmesa/.eslintignore")).be.eql(true);
     should(fs.existsSync("./blackmesa/.eslintrc.json")).be.eql(true);
